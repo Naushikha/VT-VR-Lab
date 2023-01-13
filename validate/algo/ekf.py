ae93e4ef55da18d35febc0b44e110c37e6518719
Naushikha <dev@naushikha.com>
2023-01-13 12:32:43 +0530

    Simulator: separated utils from EKF

--- validate/algo/ekf.py
+++ validate/algo/ekf.py
@@ -1,27 +1,7 @@
 import math
-import numpy as np
 import copy
-
-# Fossen;
-# The AIS course angle measurement is often unreliable. In these cases, it is recommended to
-# compute the course angle from the North - East positions(x(k-1), y(k-1)) and (x(k), y(k))
-
-
-def computeFossenChi(aisData):
-    prevX = 0
-    prevY = 0
-    correctedChi = []
-    for i in range(len(aisData["time"])):
-        newChi = math.atan2((aisData["y"][i]-prevY), (aisData["x"][i]-prevX))
-        correctedChi.append(newChi)
-        prevX = aisData["x"][i]
-        prevY = aisData["y"][i]
-    return correctedChi
-
-
-def wrapToPi(angle):
-    return (angle + np.pi) % (2 * np.pi) - np.pi
-    # https://stackoverflow.com/a/15927914
+import numpy as np
+from .utils import computeFossenChi, wrapToPi
 
 
 def extended_kalman(aisData):
