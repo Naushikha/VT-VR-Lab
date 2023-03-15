1be8e984fb7ff2ff32d3804a519f41790fa99135
Naushikha Jayawickrama <dev@naushikha.com>
2023-03-15 19:57:39 +0530

    Simulation: added cubic + flexible blending to own

--- validate/algo/own.py
+++ validate/algo/own.py
@@ -268,6 +268,77 @@ class P3:  # predicting with three AIS reports
         return self.state
 
 
+def solveCubic(p1, p2, t1, t2):
+    x1, y1 = p1
+    x2, y2 = p2
+    # Create the matrices
+    A = np.array(
+        [
+            [x1**3, x1**2, x1, 1],
+            [x2**3, x2**2, x2, 1],
+            [3 * x1**2, 2 * x1, 1, 0],
+            [3 * x2**2, 2 * x2, 1, 0],
+        ]
+    )
+    b = np.array([y1, y2, t1, t2])
+    # Solve the system of equations
+    x = np.linalg.solve(A, b)
+    return [x[0], x[1], x[2], x[3]]
+
+
+class P2_Cubic:  # predicting with two AIS reports
+    state = VesselState(0, 0, 0, 0)
+    cubicCoefX = []
+    cubicCoefY = []
+    cubicTime = 0
+
+    def __init__(self, aisReports):
+        self.state = VesselState(
+            aisReports[1].posX,
+            aisReports[1].posY,
+            aisReports[1].speed,
+            math.radians(aisReports[1].course),
+        )
+        self.cubicTime = aisReports[1].time
+        # Calculate tangents
+        tanX1 = aisReports[0].speed * math.sin(math.radians(aisReports[0].course))
+        tanX2 = aisReports[1].speed * math.sin(math.radians(aisReports[1].course))
+        tanY1 = aisReports[0].speed * math.cos(math.radians(aisReports[0].course))
+        tanY2 = aisReports[1].speed * math.cos(math.radians(aisReports[1].course))
+        self.cubicCoefX = solveCubic(
+            [aisReports[0].time, aisReports[0].posX],
+            [aisReports[1].time, aisReports[1].posX],
+            tanX1,
+            tanX2,
+        )
+        self.cubicCoefY = solveCubic(
+            [aisReports[0].time, aisReports[0].posY],
+            [aisReports[1].time, aisReports[1].posY],
+            tanY1,
+            tanY2,
+        )
+
+    def predict(self, tDelta):
+        self.cubicTime += tDelta
+        posXNext = (
+            self.cubicCoefX[0] * self.cubicTime**3
+            + self.cubicCoefX[1] * self.cubicTime**2
+            + self.cubicCoefX[2] * self.cubicTime
+            + self.cubicCoefX[3]
+        )
+        posYNext = (
+            self.cubicCoefY[0] * self.cubicTime**3
+            + self.cubicCoefY[1] * self.cubicTime**2
+            + self.cubicCoefY[2] * self.cubicTime
+            + self.cubicCoefY[3]
+        )
+        speedNext = self.state.speed  # no update to speed
+        courseNext = 0
+        stateNext = VesselState(posXNext, posYNext, speedNext, courseNext)
+        self.state = stateNext
+        return self.state
+
+
 def own_algo(aisData):
     estFreq = 60  # in Hertz
     h = 1 / estFreq
@@ -289,7 +360,7 @@ def own_algo(aisData):
                 aisData["speed"][k],
                 aisData["course"][k],
             )
-            if len(aisReports) == 3:  # Only keep last 3 reports
+            if len(aisReports) == 2:  # Only keep last 3 reports
                 aisReports.pop(0)
             aisReports.append(aisReport)
             # Init predictors
@@ -298,12 +369,12 @@ def own_algo(aisData):
             if len(aisReports) == 1:
                 predictors.append(P1(aisReports))
             elif len(aisReports) == 2:
-                predictors.append(P2(aisReports))
-            elif len(aisReports) == 3:
-                try:
-                    predictors.append(P3(aisReports))
-                except:
-                    print("bad 3-pred!")
+                predictors.append(P2_Cubic(aisReports))
+            # elif len(aisReports) == 3:
+            #     try:
+            #         predictors.append(P3(aisReports))
+            #     except:
+            #         print("bad 3-pred!")
             reportingTime = tSinceLastReport
             tSinceLastReport = 0  # reset timer
             if k < len(aisData["time"]) - 1:
@@ -315,7 +386,10 @@ def own_algo(aisData):
             stateOld = predictors[0].predict(h)
             stateNew = predictors[1].predict(h)
             # Blend between two
-            blendWeight = tSinceLastReport / reportingTime
+            blendEnd = 0.25
+            blendWeight = tSinceLastReport / (reportingTime * blendEnd)
+            if blendWeight > 1:
+                blendWeight = 1
             posXFinal = stateOld.posX + (stateNew.posX - stateOld.posX) * blendWeight
             posYFinal = stateOld.posY + (stateNew.posY - stateOld.posY) * blendWeight
             speedFinal = (
@@ -325,7 +399,7 @@ def own_algo(aisData):
                 stateOld.course + (stateNew.course - stateOld.course) * blendWeight
             )
             stateFinal = VesselState(posXFinal, posYFinal, speedFinal, courseFinal)
-            stateFinal = stateNew  # Override blending
+            # stateFinal = stateNew  # Override blending
         if len(predictors) == 1:
             stateNew = predictors[0].predict(h)
             stateFinal = stateNew
